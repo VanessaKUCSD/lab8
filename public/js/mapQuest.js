877db1e422ceeb563e897ffecfdeee2dd41fc836
Vanessa Komar <vkomar@ucsd.edu>
2021-02-25 10:48:26 -0800

    lab8

--- public/js/mapQuest.js
+++ public/js/mapQuest.js
@@ -1,3 +1,14 @@
 function initMap() {
-	// add your code here
+  L.mapquest.key = 'eQ8pPVeexTAcAr4PYXKDO2evMFuMjaPm';
+  
+  // 'map' refers to a <div> element with the ID map
+  var map = L.mapquest.map('map', {
+    center: [32.8786688,-117.2386991],
+    layers: L.mapquest.tileLayer('map'),
+    zoom: 12,
+    zoomControl: false
+  });
+
+  // Add a marker to the map
+  L.marker([32.8786688,-117.2386991]).addTo(map);
 }
